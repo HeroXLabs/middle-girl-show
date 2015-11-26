61a341afba60b62ba547c77f1d645477b8e7ec7e
Qi He <qhe@heyook.com>
2015-11-25 19:59:28 -0800

    use aws and heroku

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,8 +3,31 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
+  var env = EmberApp.env();
+  var isProductionLikeBuild = ['production', 'staging'].indexOf(env) > -1;
   var app = new EmberApp(defaults, {
-    // Add options here
+    dotEnv: {
+      clientAllowedKeys: [
+        'AWS_KEY',
+        'AWS_SECRET',
+        'AWS_BUCKET',
+        'AWS_REGION',
+        'REDIS_URL',
+        'ASSETS_URL'
+      ]
+    },
+    fingerprint: {
+      enabled: isProductionLikeBuild,
+      prepend: process.env.ASSETS_URL
+    },
+    sourcemaps: {
+      enabled: !isProductionLikeBuild,
+    },
+    minifyCSS: { enabled: isProductionLikeBuild },
+    minifyJS: { enabled: isProductionLikeBuild },
+
+    tests: process.env.EMBER_CLI_TEST_COMMAND || !isProductionLikeBuild,
+    hinting: process.env.EMBER_CLI_TEST_COMMAND || !isProductionLikeBuild
   });
 
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
