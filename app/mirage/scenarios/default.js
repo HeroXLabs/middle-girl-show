5aa95d7e903ceed56859e1ea64d1a80ea7d0a849
Qi He <qhe@heyook.com>
2015-11-26 21:25:23 -0800

    added more sites

--- app/mirage/scenarios/default.js
+++ app/mirage/scenarios/default.js
@@ -1,6 +1,12 @@
 export default function(server) {
-  server.create("site", {
-    name: "rodeo-concierge",
-    preview_url: "http://www.heyook.com"
+  let sites = [
+    { name: "cabinet-square", preview_url: "http://cabinetsquare.net" },
+    { name: "rodeo-concierge", preview_url: "http://198.199.101.184/sites/rodeo-concierge/index.html" },
+    { name: "wang-ivf", preview_url: "http://wangivfusa.com" },
+    { name: "nth", preview_url: "http://198.199.101.184/sites/nth/index.html"}
+  ];
+
+  sites.forEach( (site) => {
+    server.create("site", site);
   });
 }
