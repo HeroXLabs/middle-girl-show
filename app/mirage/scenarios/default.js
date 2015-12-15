e40517f35fcef02b4e7ab5b2d68c9ddcf6a2679c
tiffanyli <tongli1216@gmail.com>
2015-12-14 21:56:24 -0800

    added travel laikerui

--- app/mirage/scenarios/default.js
+++ app/mirage/scenarios/default.js
@@ -5,7 +5,8 @@ export default function(server) {
     { name: "wang-ivf", preview_url: "http://wangivfusa.com" },
     { name: "nth", preview_url: "http://198.199.101.184/sites/nth/index.html" },
     { name: "ty-travel", preview_url: "http://198.199.101.184/sites/tianrui-travel/index.html" },
-    { name: "topivf", preview_url: "http://198.199.101.184/sites/topivfus/index.html" }
+    { name: "topivf", preview_url: "http://198.199.101.184/sites/topivfus/index.html" },
+    { name: "lkr-travel", preview_url: "http://198.199.101.184/sites/lai-ke-rui-travel/index.html" }
   ];
 
   sites.forEach( (site) => {
